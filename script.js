d69a0101550a736b866dd8e188f06adf79ce6ebf
kamakship18 <kamakshipandoh8@gmail.com>
2023-12-21 18:33:19 +0530

    added css

--- script.js
+++ script.js
@@ -1,117 +1,141 @@
- const searchBtn = document.getElementById('search-btn');
- const mealList = document.getElementById('meal');
- const mealDetailsContent = document.querySelector('.meal-details-content');
- const recipeCloseBtn = document.getElementById('recipe-close-btn');
+// Get references to HTML elements using their IDs
+const searchBtn = document.getElementById('search-btn');
+const mealList = document.getElementById('meal');
+const mealDetailsContent = document.querySelector('.meal-details-content');
+const recipeCloseBtn = document.getElementById('recipe-close-btn');
+const searchLink = document.getElementById('searchButton')
+const searchSection = document.getElementsByClassName("meal-whole");
 
-//  event listeners
- searchBtn.addEventListener('click', getMealList);
- mealList.addEventListener('click', getMealRecipe);
- console.log(recipeCloseBtn)
- recipeCloseBtn.addEventListener('click', () => {
+// Add event listeners to buttons
+searchBtn.addEventListener('click', getMealList); // Triggered when the search button is clicked
+mealList.addEventListener('click', getMealRecipe); 
+recipeCloseBtn.addEventListener('click', () => {
+    // Close the meal details modal when the close button is clicked
     mealDetailsContent.parentElement.classList.remove('showRecipe');
 });
 
-//var submitButton = document.getElementById('search-btn'); submitButton.addEventListener('click', function() {
-//var inputValue = document.getElementById('myInput').value;
-//console.log('Input value:', inputValue);
-//});
+/*
+var submitButton = document.getElementById('search-btn');
+submitButton.addEventListener('click', function() {
+    var inputValue = document.getElementById('myInput').value;
+    console.log('Input value:', inputValue);
+});
+*/
 
+// Function to fetch and display a list of meals based on the search input
 function getMealList() {
     let searchInputText = document.getElementById('searchInput').value;
     console.log(searchInputText);
+    // Fetch data from the MealDB API based on the category search input
     fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${searchInputText}`)
         .then((response) => { return response.json() })
         .then((data) => {
             console.log(data);
             let html = "";
-            if(data.meals){
+            // Check if meals are found
+            if (data.meals) {
+                // Iterate through each meal and create HTML markup
                 data.meals.forEach(meal => {
                     html += `
-                    <div class = "meal-item" data-id = "${meal.idMeal}" >
-                        <div class ="meal-img">
-                            <img src = "${meal.strMealThumb}" alt = "food" >
+                        <div class="meal-item" data-id="${meal.idMeal}">
+                            <div class="meal-img">
+                                <img src="${meal.strMealThumb}" alt="food">
+                            </div>
+                            <div class="meal-name">
+                                <h2>${meal.strMeal}</h2>
+                                <a href="#" class="recipe-btn">Get Recipe</a>
+                            </div>
                         </div>
-                        <div class = "meal-name">
-                            <h2>${meal.strMeal}</h2>
-                            <a href = "#" class = "recipe-btn">Get Recipe</a>
-                        </div>
-                    </div>
                     `;
                 });
-            }else{
+            } else {
+                // Display a message if no meals are found
                 html = "Sorry, we didn't find any meal!";
-                mealList.classList.add('notFound')
+                mealList.classList.add('notFound');
             }
-            
-          mealList.innerHTML = html;  
+
+            // Update the HTML content of the meal list container
+            mealList.innerHTML = html;
         });
 }
 
-  // get recipe of the meal
-  function getMealRecipe(e){
+// Function to fetch and display the recipe details of a selected meal
+function getMealRecipe(e) {
     e.preventDefault();
-    if(e.target.classList.contains('recipe-btn')){
-        let mealItem = e.target.parentElement.parentElement;  
+    // Check if the clicked element has the 'recipe-btn' class
+    if (e.target.classList.contains('recipe-btn')) {
+        // Get the parent element (meal item) of the clicked button
+        let mealItem = e.target.parentElement.parentElement;
+        // Fetch the recipe details based on the meal ID
         fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealItem.dataset.id}`)
-        .then((response) => { return response.json() })
-        .then(data => mealRecipeModal(data.meals));
-            //console.log(data);
-        }
+            .then((response) => { return response.json() })
+            .then(data => mealRecipeModal(data.meals));
     }
+}
 
- // create a modal
- function mealRecipeModal(meal){
+// Function to create and display a modal with meal recipe details
+function mealRecipeModal(meal) {
     console.log(meal);
     meal = meal[0];
+    let inGrid= ""
+    // Create HTML markup for the meal recipe modal
+    for (let i = 1; i <= 20; i++) {
+        if(`${meal['strIngredient' + i]}` === "" || `${meal['strIngredient' + i]}` == null){
+        break
+        }
+    else{
+        inGrid += `<ul><li>${meal['strIngredient' + i]}</li></ul>`;
+    }
+    }    
     let html = `
-    <h2 class = "recipe-title">${meal.strMeal}</h2>
-                  <p class = "recipe-category">${meal.strCategory}</p>
-                  <div class = "recipe-instruct">
-                    <h3>Instructions:</h3>
-                    <p>${meal.strInstructions}</p>
-                  </div>
-                  <div class = "recipe-meal-img">
-                    <img src = "${meal.strMealThumb}" alt = "">
-                  </div>
-                  <div class = "recipe-link">
-                    <a href = "${meal.strYoutube}" target = "_blank">Watch Video</a>
-                  </div> 
-    `; 
+        <h2 class="recipe-title">${meal.strMeal}</h2>
+        <p class="recipe-category">${meal.strCategory}</p>
+        <div class="recipe-instruct">
+        <h2>Ingredients:</h2>
+            <p>${inGrid}</p>
+            <h3>Instructions:</h3>
+            <p>${meal.strInstructions}</p>
+            
+
+        </div>
+        <div class="recipe-meal-img">
+            <img src="${meal.strMealThumb}" alt="">
+        </div>
+        <div class="recipe-link">
+            <a href="${meal.strYoutube}" target="_blank">Watch Video</a>
+        </div>
+    `;
+    // Update the HTML content of the meal details container
     mealDetailsContent.innerHTML = html;
+    // Display the meal details modal by adding the 'showRecipe' class
     mealDetailsContent.parentElement.classList.add('showRecipe');
- } 
+}
 
-
-
- // random recipe part image
- function getRandomList() {
-     var randomList = document.getElementById('random1');
+// Function to fetch and display a random recipe image
+function getRandomList() {
+    var randomList = document.getElementById('random1');
+    // Fetch a random meal from the MealDB API
     fetch(`https://www.themealdb.com/api/json/v1/1/random.php`)
         .then((response) => { return response.json() })
         .then((data1) => {
             console.log(data1);
-            // let html = "";
-            // if(data1.meals){
-                // data1.meals.forEach(meal => {
-                    randomList.innerHTML += `
-                    <div class = "random-item" data-id = "${data1.meals[0].idMeal}" >
-                        <div class ="random-img">
-                            <img src = "${data1.meals[0].strMealThumb}" alt = "food" >
-                        </div>
-                        <div class = "random-name">
-                            <h2>${data1.meals[0].strMeal}</h2>
-                            <a href = "#" class = "recipe-btn">Get Recipe</a>
-                        </div>
+            // Append HTML markup for the random meal to the random list container
+            randomList.innerHTML += `
+                <div class="random-item" data-id="${data1.meals[0].idMeal}">
+                    <div class="random-img">
+                        <img src="${data1.meals[0].strMealThumb}" alt="food">
                     </div>
-                    `;
-                // });
-            // }else{
-            //     html = "Sorry, we didn't find any meal!";
-            //     random1.classList.add('notFound')
-            // }  
+                    <div class="random-name">
+                        <h2>${data1.meals[0].strMeal}</h2>
+                        <a href="#" class="recipe-btn">Get Recipe</a>
+                    </div>
+                </div>
+            `;
+            // Add an event listener to the random list for handling clicks
             document.getElementById("random1").addEventListener('click', getMealRecipe);
-
-            // document.getElementById("random-item").addEventListener('click', getMealRecipe);
         });
 }
-getRandomList() 
+
+// Call the function to fetch and display a random recipe image
+getRandomList();
+
